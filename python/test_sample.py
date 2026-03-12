423eb7c58b191c8c1a3d59f8c36a68ef947348cb
lawrencemwangi496-design <lawrencemwangi496@gmail.com>
2026-03-12 18:20:26 +0300

    ci: bot token

--- python/test_sample.py
+++ python/test_sample.py
@@ -11,4 +11,15 @@ def test_subtract():
     assert subtract(10, 4) == 6
 
 def test_add_negative():
-    assert add(-1, -1) == -2
+     assert add(-1, -1) == -2
+
+import os
+# telegram bot python
+token = os.getenv('BOT_TOKEN')  
+chat_id= os.gentenv('CHAT_ID')
+
+import requests 
+message = 'hello there this is matrix github action running receive three message'
+url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+r = requests.get(url)
+print((r.json()))
